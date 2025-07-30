b802a2d728cf2bc84ed44a496786d79b18a188c8
BaoYu <baoyu@for-change.cn>
2025-07-30 16:33:20 +0800

    feat: :rocket: 更改配置

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -7,6 +7,12 @@ store.use(piniaPluginPersistedstate);
 
 export default store;
 
-// export * from './modules/chat';
+export * from './modules/auth';
+export * from './modules/chat';
 export * from './modules/design';
+export * from './modules/files';
+export * from './modules/keepAlive';
+export * from './modules/loginForm';
+export * from './modules/model';
+export * from './modules/session';
 export * from './modules/user';
